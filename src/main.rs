b76b537e15c11420c667e4016a9cb169c23e97a3
Tute <mateoyastor60@gmail.com>
2022-05-22 12:26:28 -0300

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+
+
+    {
+    let area = area_of(width, height);
+    println!("Area is {}", area);
+
+    }
+
+    println!("Volume is {}", volume(width, height, depth));
+
+}
+
+fn area_of(x: i32, y:i32) -> i32 {
+
+
+    return x*y;
+}
+
+fn volume(x: i32, y:i32, z: i32) -> i32 {
+
+    //devuelvo de forma "tail expression" like a Rust programmer would do 
+    x*y*z
 }
